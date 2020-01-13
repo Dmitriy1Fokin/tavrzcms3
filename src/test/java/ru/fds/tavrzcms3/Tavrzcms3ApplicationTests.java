deb11171670f83c801fe99b2d0f7676e43e82d18
dmtriy <fokinds1@gmail.com>
2020-01-13 14:22:00 +0300

    added migration flyway

--- src/test/java/ru/fds/tavrzcms3/Tavrzcms3ApplicationTests.java
+++ src/test/java/ru/fds/tavrzcms3/Tavrzcms3ApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.fds.tavrzcms3.dictionary.TypeOfAuto;
 import ru.fds.tavrzcms3.dictionary.TypeOfCollateral;
 import ru.fds.tavrzcms3.dictionary.TypeOfSecurities;
+import ru.fds.tavrzcms3.domain.AppUser;
 import ru.fds.tavrzcms3.domain.Client;
 import ru.fds.tavrzcms3.dto.ClientDto;
 import ru.fds.tavrzcms3.converter.dtoconverter.impl.ClientLegalEntityConverterDto;
@@ -19,6 +20,8 @@ import ru.fds.tavrzcms3.repository.RepositoryAppUser;
 import ru.fds.tavrzcms3.repository.RepositoryClient;
 import ru.fds.tavrzcms3.repository.RepositoryEmployee;
 import ru.fds.tavrzcms3.repository.RepositoryLoanAgreement;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -95,6 +98,11 @@ public class Tavrzcms3ApplicationTests {
 //        System.out.println(appUser);
 //        appUser = repositoryAppUser.save(appUser);
 //        System.out.println(appUser);
+
+
+//        List<AppUser> appUserList = repositoryAppUser.findAll();
+//        appUserList.forEach(x-> x.setPassword(passwordEncoder.encode("pass")));
+//        repositoryAppUser.saveAll(appUserList);
 
     }
 
